a3ab7d2ef90ed671b22b360d8bbc69908d6da46d
fred-cook <fred@fredcook.co.uk>
2025-09-26 11:38:40 +0100

    create output folder

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from bass_map.get_coordinates import get_coords
 from bass_map.get_pdf import download_master_bass_directory
 from bass_map.parse_bass_directory import get_tables
@@ -10,7 +12,10 @@ bass_df = get_tables(filename=filename)
 permanent_bass_df = get_coords(bass_df[bass_df["status"] == "Perm"])
 guest_bass_df = get_coords(bass_df[bass_df["status"] == "Guest"])
 
+out_dir = Path("outputs")
+out_dir.mkdir(parents=True, exist_ok=True)
+
 print("Saving permanent Bass locations")
-permanent_bass_df.to_csv("permanent_locations.csv", index=False)
+permanent_bass_df.to_csv(out_dir / "permanent_locations.csv", index=False)
 print("Saving guest Bass locations")
-guest_bass_df.to_csv("guest_locations.csv", index=False)
+guest_bass_df.to_csv(out_dir / "guest_locations.csv", index=False)
