e6b26f326b937fba74cbae52d0bc33c8447702fc
fred-cook <fred@fredcook.co.uk>
2026-01-07 15:54:29 +0000

    multiple url options

--- bass_map/get_pdf.py
+++ bass_map/get_pdf.py
@@ -4,24 +4,26 @@ from datetime import datetime
 import requests
 
 
-def create_url() -> str:
+def create_urls():
     """
     Create the url for the current month's master bass directory
     """
     year = datetime.now().year
     month = datetime.now().month
-    return f"https://nationalbassdirectory.wordpress.com/wp-content/uploads/{year}/{month:02d}/bass-master-directory-{calendar.month_name[month].lower()}-{year}-1.pdf"
+    for extension in ["", "-1"]:
+        yield f"https://nationalbassdirectory.wordpress.com/wp-content/uploads/{year}/{month:02d}/bass-master-directory-{calendar.month_name[month].lower()}-{year}{extension}.pdf"
 
 
 def download_master_bass_directory(filename: str) -> None:
     """
     Save the bass directory under the given file name
     """
-    p = requests.get(create_url())
+    for url in create_urls():
+        p = requests.get(url)
 
-    if p.status_code == 200:
-        with open(filename, "wb") as fp:
-            fp.write(p.content)
-        print(f"Successfully downloaded the master bass directory to {filename}")
-    else:
-        p.raise_for_status()
+        if p.status_code == 200:
+            with open(filename, "wb") as fp:
+                fp.write(p.content)
+            print(f"Successfully downloaded the master bass directory to {filename}")
+            return
+    raise RuntimeError("Could not download the master bass directory, check the URLs")
